f957d67afe6ab8d1e95a368dada462869657a9eb
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2024-03-19 20:17:02 +0000

    Challenge 2

--- challenge2.js
+++ challenge2.js
@@ -18,3 +18,20 @@
 
 // §	Data 1: [5, 2, 4, 1, 15, 8, 3]
 // §	Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  //   console.log(humanAges);
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
