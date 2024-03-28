a8eabbae7e833c30a23a2f801cc5a8581f8b6d78
NewHellequin <128534288+NewHellequin@users.noreply.github.com>
2024-03-28 15:07:25 +0000

    array.from method

--- script.js
+++ script.js
@@ -504,3 +504,39 @@ movements.sort((a, b) => {
 movements.sort((a, b) => b - a);
 
 // console.log(movements);
+
+//////////
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays and fill method
+const x = new Array(7);
+console.log(x);
+
+// x.fill(1); fills the arrays with 1's
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (cur, i) => i + 1); // you can you _ as a throwaway aparamater instead of "cur"
+console.log(z);
+
+/////
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+});
